e0369c7f29bed12df2d329bebee6fd116db9d307
Daniel Edgecombe <edgy@qad.re>
2019-07-23 11:07:35 +0100

    RS3 Support

--- roboscript.py
+++ roboscript.py
@@ -1,11 +1,16 @@
 from collections import defaultdict
 
 
+def stringify(l, joiner=""):
+    return joiner.join(l)
+
+
 def execute(code):
     grid = defaultdict(int)
 
-    tokens = tokenize(code)
-    moves = compiler(tokens)
+    scope = {}
+    tokens = tokenize(code, scope)
+    moves = compiler(tokens, scope)
 
     position = (0, 0)
     movement = (0, 1)
@@ -24,15 +29,20 @@ def execute(code):
     return render(grid)
 
 
-def compiler(tokens):
+def compiler(tokens, scope):
     ret = []
     for token in tokens:
         if len(token) == 1:
             token = "{}1".format(token)
 
-        cmd, count = token[0], int(token[1:])
-        for i in range(count):
-            ret.append(cmd)
+        cmd, count = token[0], token[1:]
+        if cmd == "P":
+            ident = count
+            pattern_code = scope[ident]["code"]
+            ret.extend(compiler(pattern_code, scope))
+        else:
+            for i in range(int(count)):
+                ret.append(cmd)
 
     return ret
 
@@ -49,9 +59,9 @@ def render(grid):
         row = []
         for j in range(x_range[0], x_range[1] + 1):
             row.append("*" if grid[(i, j)] else " ")
-        rows.append("".join(row))
+        rows.append(stringify(row))
 
-    return "\r\n".join(rows)
+    return stringify(rows, "\r\n")
 
 
 def left(movement):
@@ -64,8 +74,56 @@ def right(movement):
     return movements[movements.index(movement) + 1]
 
 
-def tokenize(code):
+def extract_patterns(code):
     ptr = 0
+
+    ret = []
+    patterns = []
+
+    while ptr < len(code):
+        t = code[ptr]
+
+        if t == "p":
+            pattern = [t]
+            stack = 1
+            while stack > 0:
+                ptr += 1
+                if code[ptr] == "p":
+                    stack += 1
+                if code[ptr] == "q":
+                    stack -= 1
+                pattern.append(code[ptr])
+            patterns.append(stringify(pattern)[1:-1])
+        else:
+            ret.append(t)
+
+        ptr += 1
+
+    return stringify(ret), patterns
+
+
+def tokenize(code, scope):
+    ptr = 0
+
+    code, patterns = extract_patterns(code)
+
+    for pattern in patterns:
+        ident = []
+        start = 0
+        for i in range(len(pattern)):
+            if pattern[i].isdigit():
+                ident.append(pattern[i])
+            else:
+                start = i
+                break
+        ident = stringify(ident)
+
+        if ident in scope:
+            raise SyntaxError("Multiple definitions for P{}".format(ident))
+
+        scope[ident] = {
+            "code": tokenize(pattern[start:], scope),
+        }
 
     tokens = []
 
@@ -84,7 +142,7 @@ def tokenize(code):
                     stack -= 1
                 token.append(code[ptr])
                 ptr += 1
-            token = tokenize("".join(token[1:-1]))
+            token = tokenize(stringify(token[1:-1]), scope)
 
         count = []
         while ptr < len(code) and code[ptr].isdigit():
@@ -92,7 +150,7 @@ def tokenize(code):
             ptr += 1
 
         if count:
-            count = "".join(count)
+            count = stringify(count)
             if not group:
                 token.append(count)
             else:
@@ -101,6 +159,6 @@ def tokenize(code):
         if group:
             tokens.extend(token)
         else:
-            tokens.append("".join(token))
+            tokens.append(stringify(token))
 
     return tokens
